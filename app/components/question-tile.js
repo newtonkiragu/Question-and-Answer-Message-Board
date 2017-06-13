74da017213f59acfdbfbe720edd34fb98030867b
newtonkiragu <karanunewton4@gmail.com>
2017-06-13 09:50:24 +0300

    made the image to show

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,12 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   answerForm: false,
-  isImageShowing: false,
 
   actions: {
     answerFuom() {
       this.set('answerForm', true);
       this.sendAction('answerForm');
     },
+
   }
 });
