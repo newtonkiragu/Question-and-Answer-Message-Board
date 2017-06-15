c4147b960ed55499fedaaf1b9239692c4c580562
newtonkiragu <karanunewton4@gmail.com>
2017-06-15 09:49:00 +0300

    made the app be able to display data once the user clicks a question what is left is to fix the answer part

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  heading: Ember.computed('question.querry', 'question.author', function() {
+    return this.get('question.querry') + ' - ' + this.get('question.author')
+  }),
+  actions: {
+
+  }
 });
