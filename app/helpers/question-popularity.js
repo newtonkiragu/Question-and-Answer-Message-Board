4e520a32baaa2b84226a33bb60e9a9bdf56f397c
newtonkiragu <karanunewton4@gmail.com>
2017-06-16 16:18:08 +0300

    made the glypi

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -4,7 +4,7 @@ export function questionPopularity(params /*, hash*/ ) {
   var question = params[0];
 
   if (question.get('answers').get('length') >= 5) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire" id="icon"></span>');
   }
 }
 
